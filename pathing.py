4790986abd0f517d5d5c05f6f129c30f164378c8
bthimjon <bthimjon2@unl.edu>
2024-09-30 12:37:31 -0500

    finished the random path, todos in notes

--- pathing.py
+++ pathing.py
@@ -16,21 +16,9 @@ def get_test_path():
 
 
 def get_random_path():
-    #graph_data[a] = gives you graph at index a
-    #graph_data[a][0] = start node of graph a
-    #graph_data[a][length-1] = exit node of graph a
-    #graph_data[a][b][0] = x-y coordinates as tuple of point b in graph a
-    #graph_data[a][b][1] = adjacency list of point b in graph a
-    #randomly generate a based on the length of graph_data then randomly generate b based on the length of graph_data[a]
-    #then randomly generated a new point and check if its in the adjacency list 
-
     a = global_game_data.current_graph_index
     currentNode = 0
     end = len(graph_data.graph_data[a])-1
-    print("END")
-    print(end)
-    print("TARGET")
-    print(global_game_data.target_node[a])
 
     #i got it to work for hitting the target just not hitting the end
     path = startToTarget(a,currentNode,global_game_data.target_node[a])
@@ -46,19 +34,9 @@ def get_random_path():
 def startToTarget(currentGraph, currentNode, targetNode):
     path = [currentNode]
     while(currentNode != targetNode):
-        print("current node")
-        print(currentNode)
-        print("target node")
-        print(targetNode)
         adjacencyList = graph_data.graph_data[currentGraph][currentNode][1]
         currentNode =adjacencyList[random.randint(0,(len(adjacencyList)))]
         path.append(currentNode)
-        print("current path")
-        print(path)
-        if(currentNode == targetNode):
-            print("SHOULD BE DONE")
-    print("final path")
-    print(path)
     return path
 
 def get_dfs_path():
