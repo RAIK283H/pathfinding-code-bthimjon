db126e1a31998dc49521dae4ca36a45c094978d1
bthimjon <bthimjon2@unl.edu>
2024-09-30 16:13:34 -0500

    added the assert statements

--- pathing.py
+++ pathing.py
@@ -16,27 +16,32 @@ def get_test_path():
 
 
 def get_random_path():
+    assert global_game_data.current_graph_index!=-1,"The current graph index is incorrect"
     a = global_game_data.current_graph_index
     currentNode = 0
     end = len(graph_data.graph_data[a])-1
 
-    #i got it to work for hitting the target just not hitting the end
     path = startToTarget(a,currentNode,global_game_data.target_node[a])
+    assert len(path)>0,"Did not successfully hit the target node"
     currentNode = global_game_data.target_node[a]
     result = startToTarget(a,currentNode,end)
     for i in range(len(result)):
         path.append(result[i])
+    
     return path
 #b is 0 or the current index if you're keeping track of that
 #create a function that takes beginning and target 
 #while current index != target keep randomizing and go to the adjacency list of the current node
 
 def startToTarget(currentGraph, currentNode, targetNode):
+    assert targetNode>-1,"The target node is not configured properly"
+    assert currentNode>-1,"The target node is not configured properly"
     path = [currentNode]
     while(currentNode != targetNode):
         adjacencyList = graph_data.graph_data[currentGraph][currentNode][1]
         currentNode =adjacencyList[random.randint(0,(len(adjacencyList)))]
         path.append(currentNode)
+    assert len(path)>0,"No new nodes were added to the path" 
     return path
 
 def get_dfs_path():
