078b1a23336e26a7e590ac18659d38df183d3ef7
bthimjon <bthimjon2@unl.edu>
2024-10-04 10:15:04 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Pathfinding Starter Code
+My random path finder uses the current graph index, the current/starting node, and the target/ending node
+In get_random_path it gathers all of this information and feeds it into a new method called startToTarget
+    startToTarget randomly chooses nodes to visit until it hits the "target" node 
+    then it returns the path of nodes visited to get to the target node
+get_random_path then takes this information and saves it into an array 
+then calls startToTarget to get from the target node that the last path ended on to the end of the graph
+    startToTarget does the same thing using the new node information
+get_random_path then saves this into an array and appends each node onto the path
+then the path is returned 
+
+In scoreboard I added the time it takes for the player to move to each individual node
+This allows players to see how long in real time it takes for the player to travel between nodes
--- pathing.py
+++ pathing.py
@@ -17,25 +17,29 @@ def get_test_path():
 
 def get_random_path():
     assert global_game_data.current_graph_index!=-1,"The current graph index is incorrect"
+    #gets the current graph index and sets the beginning and ending nodes
     a = global_game_data.current_graph_index
     currentNode = 0
     end = len(graph_data.graph_data[a])-1
 
+    #create a path using the random path calculated to the target node
     path = startToTarget(a,currentNode,global_game_data.target_node[a])
     assert len(path)>0,"Did not successfully hit the target node"
+    #sets the current node equal to the target node(where the path ended)
     currentNode = global_game_data.target_node[a]
+    #gets the remaining path from the target to the end node and append to the path
     result = startToTarget(a,currentNode,end)
     for i in range(len(result)):
         path.append(result[i])
     return path
-#b is 0 or the current index if you're keeping track of that
-#create a function that takes beginning and target 
-#while current index != target keep randomizing and go to the adjacency list of the current node
 
+#takes in the current graph index, the starting node, and the target end node
 def startToTarget(currentGraph, currentNode, targetNode):
     assert targetNode>-1,"The target node is not configured properly"
     assert currentNode>-1,"The target node is not configured properly"
+    #starts an array for the path with the beginning node
     path = [currentNode]
+    #continues to choose a random node until it is the target node
     while(currentNode != targetNode):
         adjacencyList = graph_data.graph_data[currentGraph][currentNode][1]
         currentNode =adjacencyList[random.randint(0,(len(adjacencyList)))]
