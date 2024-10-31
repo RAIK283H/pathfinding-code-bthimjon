cc106807ec1e4a35b8cde0771716e564ec6d709c
bthimjon <bthimjon2@unl.edu>
2024-10-31 14:08:08 -0500

    yup

--- main2.py
+++ main2.py
@@ -1,5 +1,10 @@
 import graph_data
 import permutation
 import global_game_data
+import main
 
-permutation.find_hami_cycles()
+for i in range (len(graph_data.graph_data)):
+    permutation.find_hami_cycles()
+    print(i)
+    print(len(graph_data.graph_data))
+    main.change_graph()
