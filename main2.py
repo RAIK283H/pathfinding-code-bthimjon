a41cefe392b203aeb5bf61a68a8b47cea44f4cff
bthimjon <bthimjon2@unl.edu>
2024-11-01 12:38:40 -0500

    added comments

--- main2.py
+++ main2.py
@@ -3,8 +3,9 @@ import permutation
 import global_game_data
 import main
 
+#goes through all the graphs and finds the hamiltonian cycles
 for i in range (len(graph_data.graph_data)):
     permutation.find_hami_cycles()
-    print(global_game_data.current_graph_index)
+    #changes the graphs without using pyglet
     main.change_graph()
     
--- permutation.py
+++ permutation.py
@@ -2,10 +2,12 @@ from itertools import permutations
 import global_game_data
 import graph_data
 
+#creates the permutations for the graph
 def sjt_permutation(graphIndex):
     elements = range(1, len(graph_data.graph_data[graphIndex]) - 1)
     return list(permutations(elements))
 
+#checks if the permutation for the graph is a valid hamiltonian cycle
 def is_valid_hami_cycle(permutation, end, graphIndex):
     cycle = [0] + list(permutation) + [end]
     
@@ -15,10 +17,12 @@ def is_valid_hami_cycle(permutation, end, graphIndex):
 
         adj_nodes = graph_data.graph_data[graphIndex][node][1]
         
+        #returns false if the nodes are not connected
         if next_node not in adj_nodes:
             return False
     return True 
 
+#finds all the hamiltonian cycles for the graph
 def find_hami_cycles():
     index = global_game_data.current_graph_index
     end = len(graph_data.graph_data[index]) - 1
@@ -28,10 +32,12 @@ def find_hami_cycles():
         if is_valid_hami_cycle(perm, end, index):
             valid_cycles.append([0] + list(perm) + [end])
 
+#prints all the valid hamiltonian cycles
     if valid_cycles:
         for cycle in valid_cycles:
             print("Valid Hamiltonian Cycle(s):", cycle)
         return valid_cycles
+#returns -1 and prints that there are no valid cycles
     else:
         print("No valid Hamiltonian cycle exists")
         return -1
